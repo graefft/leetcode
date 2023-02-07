20c2b213a550d6c145e6089a3b6f56eefd7cc1cf
graefft <rustygraeff@gmail.com>
2023-02-07 15:57:15 -0800

    Update Time & Space

--- 0001_TwoSum.py
+++ 0001_TwoSum.py
@@ -1,7 +1,11 @@
 '''
--------------
---- O(n) ----
--------------
+--------------------
+--- TIME -----------
+--- O(n) - 70ms ----
+--------------------
+--- SPACE-----------
+--- O(n) - 15.2mb --
+--------------------
 '''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -14,12 +18,15 @@ class Solution:
 
 
 '''
---------------
---- O(n^2) ---
---------------
+--------------------
+--- TIME -----------
+--- O(n^2) -3858ms--
+--------------------
+--- SPACE-----------
+--- O(n) - 14.9mb --
+--------------------
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
         for i in range(0, len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
